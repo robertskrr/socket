d4b98047e45af6cbbbfecf54b96ee2d46129f3d0
Robert Esquerre <esquerre.valiente.robert.ernesto@iescamas.es>
2026-01-23 19:22:20 +0100

    Mejora 1: cierre de recursos y conexiones

--- RandomSocket/ClientSocket/src/main/java/com/socket/AppCliente.java
+++ RandomSocket/ClientSocket/src/main/java/com/socket/AppCliente.java
@@ -11,43 +11,45 @@ import java.rmi.UnknownHostException;
  * Hello world!
  *
  */
-public class AppCliente 
-{
-	static final int  PORT = 7777;
-	
-    public static void main( String[] args )
-    {
-    	
-        try {
-        	//conectamos con el servidor
-        	Socket socket = new Socket("localhost",PORT);
-        	
-        	//Para enviar datos al server
-        	PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
-        	
-        	//Para recibir respuestas del servidor
-        	BufferedReader entradaSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        	//Leer los datos introducidos por consola
-        	BufferedReader entradaConsola = new BufferedReader(new InputStreamReader(System.in));
-        	
-        	System.out.println("<Cliente>Inserte un número: ");
-        	//Leemos de consola y enviamos al server
-        	salida.println(entradaConsola.readLine());
-        	
-        	String datoRec;
-        	while((datoRec = entradaSocket.readLine()) != null){
-        		//Mostrar el dato recibido por consola
-        		System.out.println(datoRec);
-        		// Leer la consola y enviar al server
-        		salida.println(entradaConsola.readLine());
-        	}
-        	
-        	
-        }catch(UnknownHostException ex) {
-        	
-        } catch (IOException e) {
-			
+public class AppCliente {
+	static final int PORT = 7777;
+
+	public static void main(String[] args) {
+
+		try {
+			// conectamos con el servidor
+			Socket socket = new Socket("172.21.76.57", PORT);
+
+			// Para enviar datos al server
+			PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+
+			// Para recibir respuestas del servidor
+			BufferedReader entradaSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			// Leer los datos introducidos por consola
+			BufferedReader entradaConsola = new BufferedReader(new InputStreamReader(System.in));
+
+			System.out.println("<Cliente>Inserte un número: ");
+			// Leemos de consola y enviamos al server
+			salida.println(entradaConsola.readLine());
+
+			String datoRec;
+			while ((datoRec = entradaSocket.readLine()) != null) {
+				// Mostrar el dato recibido por consola
+				System.out.println(datoRec);
+				// Leer la consola y enviar al server
+				salida.println(entradaConsola.readLine());
+			}
+
+			// Cerramos todo
+			salida.close();
+			entradaSocket.close();
+			entradaConsola.close();
+			socket.close();
+
+		} catch (UnknownHostException ex) {
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    }
+	}
 }
--- RandomSocket/ServerSocket/src/main/java/com/socket/AppServerSocket.java
+++ RandomSocket/ServerSocket/src/main/java/com/socket/AppServerSocket.java
@@ -13,73 +13,77 @@ import java.util.Random;
  * Hello world!
  *
  */
-public class AppServerSocket 
-{
+public class AppServerSocket {
 	private final static int PORT = 7777;
 	private static int numGen;
-    public static void main( String[] args ) 
-    {
-    	//Generar número
-    	numGen = (new Random()).nextInt(10)+1;
-    	
-    	try {
-    		ServerSocket srvSock = new ServerSocket(PORT);
-    		System.out.println("ServerSocket en puerto: " + PORT);
 
-    		//Abrimos el socket y escuchamos
-    		Socket client = srvSock.accept();
-    		mostrarInfoCliente(client);
-    		
-    		
-    		//Para mandar datos al cliente >>>
-    		PrintWriter salida = new PrintWriter(client.getOutputStream(), true);
-    		//Para recibir datos al cliente <<<
-    		BufferedReader entrada = new BufferedReader(new InputStreamReader(client.getInputStream()));
-    	
-    		
-    		String datoRec, datoEnv;
-    		
-    		//leeremos todos los mensajes recibidos
-    		//comprobamos si es el número mágico
-    		while((datoRec = entrada.readLine())!= null) {
-  
-    			datoEnv = checkNumero(datoRec);
-    			
-    			//Retornamos al cliente el resultado 
-    			//de la comprobación
-    			salida.println(datoEnv);
-    		}
-    		
-    	}catch(IOException e) {
-    		System.err.println("Problemas en el socket");
-    		e.printStackTrace();
-    		System.exit(1);
-    	}
-    }
-	private static void mostrarInfoCliente(Socket client) {
-			// Obtener la IP del cliente
-			InetAddress clientAddress = client.getInetAddress();
-			String clientIP = clientAddress.getHostAddress();
-			String hostName = clientAddress.getHostName();
-		
-			System.out.println("IP:" + clientIP + ", HostName: "+ hostName);
+	public static void main(String[] args) {
+		// Generar número
+		numGen = (new Random()).nextInt(10) + 1;
+
+		try {
+			ServerSocket srvSock = new ServerSocket(PORT);
+			System.out.println("ServerSocket en puerto: " + PORT);
+
+			// Abrimos el socket y escuchamos
+			Socket client = srvSock.accept();
+			mostrarInfoCliente(client);
+
+			// Para mandar datos al cliente >>>
+			PrintWriter salida = new PrintWriter(client.getOutputStream(), true);
+			// Para recibir datos al cliente <<<
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+			String datoRec, datoEnv;
+
+			// leeremos todos los mensajes recibidos
+			// comprobamos si es el número mágico
+			while ((datoRec = entrada.readLine()) != null) {
+
+				datoEnv = checkNumero(datoRec);
+
+				// Retornamos al cliente el resultado
+				// de la comprobación
+				salida.println(datoEnv);
+			}
+
+			// Cerramos todo
+			salida.close();
+			entrada.close();
+			srvSock.close();
+			client.close();
+
+		} catch (IOException e) {
+			System.err.println("Problemas en el socket");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
+
+	private static void mostrarInfoCliente(Socket client) {
+		// Obtener la IP del cliente
+		InetAddress clientAddress = client.getInetAddress();
+		String clientIP = clientAddress.getHostAddress();
+		String hostName = clientAddress.getHostName();
+
+		System.out.println("CLIENTE CONECTADO -- IP:" + clientIP + ", HostName: " + hostName);
+	}
+
 	private static String checkNumero(String datoRec) {
 		try {
 			int numero = Integer.parseInt(datoRec);
-			
-			if(numero > numGen) {
+
+			if (numero > numGen) {
 				return "<server>El número es mayor que el número mágico";
-			}else if(numero < numGen) {
+			} else if (numero < numGen) {
 				return "<server>El número es menor que el número mágico";
-			}else {
+			} else {
 				return "<server>Ha adivinado el número";
 			}
-			
-			
-		}catch(NumberFormatException e) {
+
+		} catch (NumberFormatException e) {
 			return "<Server>Por favor, introduzca un número";
 		}
-		
+
 	}
 }
